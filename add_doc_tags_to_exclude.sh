bddd16310b2da24775e7a64661b742b1f5f67c47
Gerd Wachsmuth <gerd.wachsmuth@mathematik.tu-chemnitz.de>
2011-03-24 10:13:43 +0100

    Added the file add_doc_tags_to_exclude.sh

---
+++ add_doc_tags_to_exclude.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# copied from https://github.com/vim-scraper/vim-scraper/issues/closed#issue/13
+# adds "doc/tags" to the files "bundle/*/.git/info/exclude"
+# => git will not report, that the doc/tags was added in a bundle and is untracked
+
+for i in bundle/*/.git/info/exclude; do
+	grep -q "doc/tags" "$i" || echo "doc/tags" >> "$i";
+done
